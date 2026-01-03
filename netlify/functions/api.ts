b367fdd827ff3947141d8604cf9b942c475b9419
GuilhermeBasto <jgsbasto@gmail.com>
2026-01-03 16:27:37 +0000

    fix: deploy on netfly

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,6 +1,8 @@
 import serverless from 'serverless-http';
 import app from '../../dist/app';
 
-const handler = serverless(app);
+const handler = serverless(app, {
+  binary: ['image/*', 'application/pdf', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'],
+});
 
 export { handler };
